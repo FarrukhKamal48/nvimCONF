997d0cf13cc64d2bd649793da3e7f3b8275b00ff
Farrukh Kamal <farrukhkamal429@gmail.com>
2022-07-11 00:00:10 +0500

    Added some convenient keybinds for save and quit

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,11 +1,15 @@
 -- Shorten function name
 local keymap = vim.keymap.set
+
+
 -- Silent keymap option
 local opts = { silent = false }
+
 
 --Remap space as leader key
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
+
 
 -- Modes
 --   normal_mode = "n",
@@ -15,6 +19,7 @@ vim.g.mapleader = " "
 --   term_mode = "t",
 --   command_mode = "c",
 
+
 -- Normal --
 -- Better window navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
@@ -22,20 +27,25 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
+
 -- Change key map for cycling search term
 keymap("n", "b", "n", opts)
+
 
 -- -- Change key map for mark
 -- keymap("n", "`", "m", opts)
 -- keymap("n", "~", "'", opts)
 
+
 -- Move to START and END of LINE
 keymap("n", "n", "0", opts)
 keymap("n", "m", "$", opts)
 
+
 -- Move to TOP and BOTTOM of FILE
 keymap("n", "<S-n>", "gg", opts)
 keymap("n", "<S-m>", "<S-g>", opts)
+
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
@@ -43,20 +53,25 @@ keymap("n", "<C-Down>", ":resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
+
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
+
 -- Clear highlights
 keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
 
+
 -- Close and Save buffers
-keymap("n", "<C-q>", "<cmd>Bdelete!<CR>", opts)
-keymap("n", "<C-s>", "<cmd>w!<CR>", opts)
+keymap("n", "q", "<cmd>Bdelete!<CR>", opts)
+keymap("n", "s", "<cmd>w!<CR>", opts)
+
 
 -- Close and Save all
-keymap("n", "<leader><C-q>", "<cmd>qa!<CR>", opts)
-keymap("n", "<leader><C-s>", "<cmd>wa!<CR>", opts)
+keymap("n", "<C-q>", "<cmd>qa!<CR>", opts)
+keymap("n", "<C-s>", "<cmd>wa!<CR>", opts)
+
 
 -- Save, Compile and Run (C/C++)
                                                            -- take the full path, and remove the file part.
@@ -65,26 +80,32 @@ keymap("n", "<leader><C-s>", "<cmd>wa!<CR>", opts)
                                                          --|| extention and then apend it to the path string. 
 keymap("n","<A-;>", ':w!<CR>:TermExec cmd="gcc % -o %:r && %:p:h/./%:t:r"<CR>', opts)
 
+
 -- Better paste
 keymap("v", "p", '"_dP', opts)
+
 
 -- Insert --
 -- Press jk fast to escape
 keymap("i", "jk", "<ESC>", opts)
 keymap("i", "kj", "<ESC>", opts)
 
+
 -- Visual --
 -- Stay in indent mode
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
+
 
 -- Plugins --
 
 -- NvimTree
 keymap("n", "<leader>e", ":NvimTreeToggle<CR> :NvimTreeRefresh<CR>", opts)
 
+
 -- ToogleTerm
 -- keymap("n", "<C-/>", ":ToggleTermToggleAll<CR>", opts)
+
 
 -- Telescope
 keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
@@ -92,12 +113,15 @@ keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
 keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
 keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
 
+
 -- Git
 keymap("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
+
 
 -- Comment
 keymap("n", "<leader>cc", "<cmd>lua require('Comment.api').toggle_current_linewise()<CR>", opts)
 keymap("x", "<leader>cc", '<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>')
+
 
 -- DAP
 keymap("n", "<leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<cr>", opts)
